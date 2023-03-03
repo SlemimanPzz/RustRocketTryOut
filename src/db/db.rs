0031337317edcd71ea98fbd54ede2679c4490dcd
SlemimanPzz <emilianoa@ciencias.unam.mx>
2023-03-02 23:23:13 -0600

    Init

---
+++ src/db/db.rs
@@ -0,0 +1,59 @@
+use rocket::futures::{TryFutureExt, TryStreamExt};
+use rocket_db_pools::{Database, Connection};
+use rocket_db_pools::sqlx::{self, Row, database};
+use rocket::fairing::AdHoc;
+
+
+pub fn stage() -> AdHoc {
+    AdHoc::on_ignite("SQLx Stage", |rocket| async {
+        rocket.attach(Db::init())
+            .mount("/db", routes![entry,read])
+    })
+}
+
+#[derive(Database)]
+#[database("sqlx")]
+pub struct Db(sqlx::SqlitePool);
+
+
+#[get("/read/<id>")]
+async fn read(mut db: Connection<Db>, id: i64) -> String {
+   let reg = sqlx::query(&format!("SELECT Nombre FROM Registro WHERE entry_id = {};", id)).bind(id)
+       .fetch_one(&mut *db).await
+       .and_then(|r| Ok(r.try_get_unchecked(0)?))
+       .ok();
+
+   match reg {
+       Some(x) => x,
+       None => String::from("Not Found")
+   }
+
+
+}
+
+#[get("/entry/<person>")]
+async fn entry(person : String, mut db : Connection<Db>) ->  String {
+    let entry_id = sqlx::query(&format!("INSERT INTO Registro (Nombre) VALUES ('{}') RETURNING entry_id;", person))
+    .bind(person)
+    .fetch_one(&mut *db).await
+    .and_then(|r| Ok(r.try_get_unchecked(0)?))
+    .ok();
+
+    match entry_id {
+        Some(x) => x,
+        None => String::from("Some error")
+    }
+}
+
+
+// #[get("/all")]
+// async fn all(mut db : Connection<Db>) -> Result<Json<Vec<String>>> {
+//     let x = sqlx::query("SELECT * FROM Registro;")
+//     .fetch_all(&mut *db)
+//     .map_ok(|r| r)
+//     .try_collect::<Vec<String>>()
+//     .await?;
+
+//     Ok(Json(x))
+
+// }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,62 @@
-fn main() {
-    println!("Hello, world!");
+
+#[macro_use] extern crate rocket;
+use rocket::State;
+use std::sync::Mutex;
+use rocket_db_pools::{Database, Connection};
+use rocket_db_pools::sqlx::{self, Row, database};
+
+
+#[path="./db/db.rs"]
+mod db;
+
+use db::{Db, stage};
+
+
+#[get("/")]
+fn hello()  -> &'static str{
+    "Hello Rocket 🚀"
+}
+
+
+#[get("/all")]
+fn all_vector(vector : &State<Mutex<Vec<i32>>>) -> String {
+    let inner = vector.inner();
+    let binding = inner.lock().unwrap();
+    let mut string_vec = String::from("");
+    for x in binding.to_vec() {
+        if string_vec.is_empty() {
+            string_vec.push_str(&x.to_string());
+            continue
+        }
+        string_vec.push_str(", ");
+        string_vec.push_str(&x.to_string())
+    }
+    return string_vec;
+}
+
+#[get("/<index>")]
+fn vec(index : usize, elvector : &State<Mutex<Vec<i32>>>) -> String {
+    let inner = elvector.inner();
+    let mut binding = inner.lock().unwrap();
+    binding.push(index.try_into().unwrap());
+    print!("Added {} in pos {}.🌊", index, binding.len());
+    let num = binding.get(index);
+    match num {
+        Some(x) => {format!("Ok {}", x)},
+        None => String::from("Invalid Index 😈")
+    }
+
+
+}
+
+#[launch]
+fn rocket()  -> _ {
+
+
+    rocket::build()
+        .manage(Mutex::new(vec![0,1,2,3,4,10]))
+        .manage(Db::init())
+        .mount("/hello", routes![hello])
+        .mount("/vec", routes![vec, all_vector])
+        .attach(stage())
 }
